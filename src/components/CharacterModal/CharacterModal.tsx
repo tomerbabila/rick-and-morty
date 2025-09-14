00040e307de49ffa59d52167588e67ba42282a7b
tomerbabila <tomer.babila@gmail.com>
2025-09-14 20:23:38 +0300

    Add css for responsiveness

--- src/components/CharacterModal/CharacterModal.tsx
+++ src/components/CharacterModal/CharacterModal.tsx
@@ -14,26 +14,28 @@ export default function CharacterModal({ character }: CharacterModalProps) {
   return (
     <div className={styles.modal}>
       <img className={styles.avatar} src={character.image} alt={character.name} />
-      <div className={styles.details}>
-        <h3>Details:</h3>
-        <CharacterModalDetail title='Status' value={character.status} />
-        <CharacterModalDetail title='Species' value={character.species} />
-        <CharacterModalDetail title='Gender' value={character.gender} />
-        <CharacterModalDetail title='No. of Episodes' value={character.episode.length} />
-      </div>
-      <div className={styles.details}>
-        <h3>Origin:</h3>
-        {loading && <div>Loading origin...</div>}
-        {error && <div>Error loading origin: {error.message}</div>}
-        {locationData && !loading && !error ? (
-          <>
-            <CharacterModalDetail title='Name' value={locationData?.name} />
-            <CharacterModalDetail title='Type' value={locationData?.type} />
-            <CharacterModalDetail title='Dimension' value={locationData?.dimension} />
-          </>
-        ) : (
-          <div className={styles.details}>No origin data available.</div>
-        )}
+      <div className={styles.info}>
+        <div className={styles.details}>
+          <h3>Details:</h3>
+          <CharacterModalDetail title='Status' value={character.status} />
+          <CharacterModalDetail title='Species' value={character.species} />
+          <CharacterModalDetail title='Gender' value={character.gender} />
+          <CharacterModalDetail title='No. of Episodes' value={character.episode.length} />
+        </div>
+        <div className={styles.details}>
+          <h3>Origin:</h3>
+          {loading && <div>Loading origin...</div>}
+          {error && <div>Error loading origin: {error.message}</div>}
+          {locationData && !loading && !error ? (
+            <>
+              <CharacterModalDetail title='Name' value={locationData?.name} />
+              <CharacterModalDetail title='Type' value={locationData?.type} />
+              <CharacterModalDetail title='Dimension' value={locationData?.dimension} />
+            </>
+          ) : (
+            <div className={styles.details}>No origin data available.</div>
+          )}
+        </div>
       </div>
     </div>
   );
