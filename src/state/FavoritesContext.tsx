008d241cd882099233b0d5f3ec8144611aac3528
tomerbabila <tomer.babila@gmail.com>
2025-09-15 12:42:26 +0300

    Use local storage to store favorites

--- src/state/FavoritesContext.tsx
+++ src/state/FavoritesContext.tsx
@@ -1,11 +1,16 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useEffect, useReducer } from 'react';
 import { Character } from 'api/client.types';
 
+const LOCAL_STORAGE_KEY = 'rick-and-morty-favorites';
+
 type FavoritesState = Record<number, Character>; // e.g. { [id]: Character }
-type Action = { type: 'toggle'; character: Character };
+type Action = { type: 'toggle'; character: Character } | { type: 'load'; payload: FavoritesState };
 
 function reducer(state: FavoritesState, action: Action): FavoritesState {
   switch (action.type) {
+    case 'load': {
+      return action.payload;
+    }
     case 'toggle': {
       const { id } = action.character;
       const newState = { ...state };
@@ -18,7 +23,7 @@ function reducer(state: FavoritesState, action: Action): FavoritesState {
 
       return newState;
     }
-    // TODO: add load, remove actions
+    // TODO: add removeAll actions
     default:
       return state;
   }
@@ -32,10 +37,22 @@ const Ctx = createContext<{ favorites: Character[]; toggle: (c: Character) => vo
 export const FavoritesProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, {});
 
+  useEffect(() => {
+    // Load from local storage
+    const raw = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const payload = raw ? JSON.parse(raw) : {};
+    dispatch({ type: 'load', payload });
+  }, []);
+
+  useEffect(() => {
+    // Save to local storage
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
+  }, [state]);
+
   const value = {
     favorites: Object.values(state),
     toggle: (c: Character) => dispatch({ type: 'toggle', character: c }),
-    // TODO: add load, remove, has functions
+    // TODO: add removeAll, has functions
   };
 
   return <Ctx.Provider value={value}>{children}</Ctx.Provider>;
